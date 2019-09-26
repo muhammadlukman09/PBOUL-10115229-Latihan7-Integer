d3d2adc3e8e08f271223079d8cea38a294e609cf
fakhri ramdani <muhammadlukman09@gmail.com>
2019-09-26 10:08:09 +0700

    project ketujuh

---
+++ src/pboul/pkg10115229/latihan7/integer/PBOUL10115229Latihan7Integer.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboul.pkg10115229.latihan7.integer;
+
+/**
+ *
+ * @author Lukman
+ * NIM          : 10115229
+ * NAMA         : MUHAMMAD LUKMAN HAKIM
+ * HARITANGGAL  : SABTU, 21 SEPTEMBER 2019
+ * LOG
+ */
+public class PBOUL10115229Latihan7Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        byte b;
+        short s;
+        int i;
+        long l;
+        
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        
+        System.out.println("byte : " + b);
+        System.out.println("short : " + s);
+        System.out.println("int : " + i);
+        System.out.println("long : " + l);
+    }
+    
+}
